98c4d7128d2b4aef7b86798a27ae61876a3e775d
yulelily <124838371+yulelily@users.noreply.github.com>
2025-05-17 00:09:23 -0400

    Add build

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ To build the project run:
 
 ```bash
 ng build
+ng build --configuration production --base-href "https://yulelily.github.io/yumearu/"
 ```
 
 This will compile your project and store the build artifacts in the `dist/` directory. By default, the production build optimizes your application for performance and speed.
--- src/app/lib/lib.component.ts
+++ src/app/lib/lib.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { ContentComponent } from "../content/content.component";
-import { ErrorComponent } from "../error/error.component";
 
 @Component({
   selector: 'app-lib',
-  imports: [NavBarComponent, ContentComponent, ErrorComponent],
+  imports: [NavBarComponent, ContentComponent],
   templateUrl: './lib.component.html',
   styleUrl: './lib.component.css'
 })
