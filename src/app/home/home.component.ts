ea88ca6afce446689006a699bab6b153839ca9c7
yulelily <124838371+yulelily@users.noreply.github.com>
2025-05-16 22:15:33 -0400

    Add spoiler tags
    added component for spoiler tagged text and spoiler tagged image,
    spoiler toolbars coming soon(tm)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { ContentComponent } from "../content/content.component";
+import { SpoilerTextComponent } from "../spoiler-text/spoiler-text.component";
+import { SpoilerImageComponent } from "../spoiler-image/spoiler-image.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, ContentComponent],
+  imports: [NavBarComponent, ContentComponent, SpoilerTextComponent, SpoilerImageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
 }
